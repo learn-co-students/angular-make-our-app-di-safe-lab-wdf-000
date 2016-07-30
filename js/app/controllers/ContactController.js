579b94a9ac24b940ecb08a0b7aa35df7bda8b27c
uu <giladbarkan@live.com>
2016-07-30 15:22:17 -0400

    seems working in the browser

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,11 +1,13 @@
-function ContactController($scope, $timeout) {
-	$scope.name = 'Bill Gates';
+function ContactController(random, words) {
+	random.name = 'Bill Gates';
 
-	$timeout(function () {
-		$scope.name = 'Steve Jobs';
+	words(function () {
+		random.name = 'Steve Jobs';
 	}, 5000);
 }
 
 angular
 	.module('app')
 	.controller('ContactController', ContactController);
+
+ContactController.$inject = ['$scope','$timeout'];
